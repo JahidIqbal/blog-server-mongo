cfcbaaec0339f6f4b4254d85ef57d00f56e40a3e
hrid620 <hridishere@gmail.com>
2022-01-30 14:35:12 +0600

    please

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
-
-
 async function run() {
     try {
         await client.connect();
@@ -102,7 +99,7 @@ async function run() {
 
 
 
-        // post users
+        // post user
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
